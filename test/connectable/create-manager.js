a3458992e55ed41aad5c7a0cfb311b0b1c5dd8da
Scott Gress <scottmgress@gmail.com>
2017-01-31 18:01:41 -0600

    Fix tests

--- test/connectable/create-manager.js
+++ test/connectable/create-manager.js
@@ -3,17 +3,29 @@ var Pack = require('../../');
 
 describe('Connectable ::', function() {
   describe('Create Manager', function() {
-    it('should validate the connection string has a protocol', function(done) {
+    it('should work without a protocol in the connection string', function(done) {
       Pack.createManager({
         connectionString: 'localhost:27017/mppg'
       })
       .exec(function(err) {
-        assert(err);
-        assert.equal(err.exit, 'malformed');
-
+        if (err) {
+          return done(err);
+        }
         return done();
       });
     });
+
+    it('should not work with an invalid protocol in the connection string', function(done) {
+      Pack.createManager({
+        connectionString: 'foobar://localhost:27017/mppg'
+      })
+      .exec(function(err) {
+        assert(err);
+        assert.equal(err.exit, 'malformed');
+        return done();
+      });
+    });
+
 
     it('should successfully return a Mongo Server instance', function(done) {
       // Needed to dynamically get the host using the docker container
